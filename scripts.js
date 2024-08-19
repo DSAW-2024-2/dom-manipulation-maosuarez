016c9c5e216852cebb2f7db235458e555ba68ffa
Mao Suarez <maosuarezbarrer@gmail.com>
2024-08-19 12:18:24 -0500

    First Commit

--- scripts.js
+++ scripts.js
@@ -7,20 +7,36 @@ const lista = [
 ];
 let image = 0;
 
-const btnDer = document.getElementById("btnDer");
-const btnIzq = document.getElementById("btnIzq");
-const Idimage = document.getElementById("image");
-const container = document.querySelector(".contenedor");
+const carousel = document.querySelector(".carousel");
+const btnIzq = document.querySelector(".btn-izq");
+const btnDer = document.querySelector(".btn-der");
+
+function crearItems() {
+  for (let i = 0; i < lista.length; i++) {
+    let items = document.createElement("div");
+    items.setAttribute("class", "carousel-item");
+    if (i == 0) {
+      items.classList.add("show");
+    }
+
+    let img = document.createElement("img");
+    img.src = lista[i];
+    img.alt = "Imagen de Carrusel" + (i + 1);
+
+    items.appendChild(img);
+
+    carousel.appendChild(items);
+  }
+}
 
 btnDer.addEventListener("click", function () {
   if (image + 1 < lista.length) {
     btnIzq.style.display = "block";
     image++;
-    Idimage.src = lista[image];
     if (image + 1 == lista.length) {
       btnDer.style.display = "none";
     }
-    updateCarousel();
+    updateCarousel(image);
   }
 });
 
@@ -28,15 +44,25 @@ btnIzq.addEventListener("click", function () {
   if (image > 0) {
     btnDer.style.display = "block";
     image -= 1;
-    Idimage.src = lista[image];
     if (image == 0) {
       btnIzq.style.display = "none";
     }
-    updateCarousel();
+    updateCarousel(image);
   }
 });
 
-function updateCarousel() {
-  container.style.transform = `translateX(${Idimage * -100}%)`;
-  Idimage.style.opacity = index === image ? 1 : 0;
+function updateCarousel(index) {
+  const carouselItems = document.querySelectorAll(".carousel-item");
+  carouselItems.forEach((item, i) => {
+    item.classList.remove("show");
+    if (i === index) {
+      item.classList.add("show");
+    }
+  });
+  const offset = -index * 100;
+  carousel.style.transform = `translateX(${offset}%)`;
 }
+
+//Codigo Ejecutable
+crearItems();
+updateCarousel(image);
